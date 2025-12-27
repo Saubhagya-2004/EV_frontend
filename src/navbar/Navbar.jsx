1daa354cbd1028acc5e8cde926e60a6876a50778
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2025-12-27 11:12:33 +0530

    changes

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="w-full bg-gradient-to-r from-green-200 to-blue-300 text-white px-6 py-3 flex justify-between items-center fixed top-0 left-0 z-50">
+    <nav className="w-full bg-gradient-to-r from-green-200 to-blue-300 text-white px-6 py-3 flex justify-between items-center top-0 left-0 z-50">
       <Link to="/home" className="text-xl font-bold">
         <div className="p-3 bg-green-600 rounded-xl shadow-lg">
           <RiFlashlightFill className="text-white text-2xl" />
@@ -24,12 +24,12 @@ const Navbar = () => {
             <span className="hidden sm:block">
               Hi, <b>{user.name}</b>
             </span>
-            <Link to="/find-cpo" className="hover:text-green-400 duration-500">
+            <Link to="/find-cpo" className="hover:bg-green-600 px-3 py-1 rounded-2xl transition duration-500 ">
               Find CPO
             </Link>
             <button
               onClick={handleLogout}
-              className="bg-red-500 px-3 py-1 rounded"
+              className="bg-red-500 px-3 py-1 rounded-2xl"
             >
               Logout
             </button>
